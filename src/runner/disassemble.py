8be6260fd4fbcb98ee0e6e695f181a965fab9c0c
Welby Seely <welbyseely@gmail.com>
2026-06-23 18:21:14 -0400

    Embedding generation for all 3 models works

--- src/runner/disassemble.py
+++ src/runner/disassemble.py
@@ -1,10 +1,14 @@
+import gc
 import os.path, logging, subprocess
 from itertools import product
 from pathlib import Path
 
+import torch
+from sentence_transformers import SentenceTransformer
+
 from src.runner.compiler import GnuCompilerCollection, OptimizationLevel, Msvc, Clang
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
 from src.runner.validation import validate_response
 
@@ -41,25 +45,42 @@ def produce_diassemblies():
     source_paths = get_source_paths()
 
     compilers = [GnuCompilerCollection(), Msvc(), Clang()]
-
+    diassembly_labels = []
+    disassemblies = []
     try:
-        for source_path, optimization_level, compiler in product(source_paths, [o for o in OptimizationLevel], compilers):
+        for compiler, source_path, optimization_level in product(compilers, source_paths,
+                                                                 [o for o in OptimizationLevel]):
             object_file = compiler.compile(source_path,
-                                      object_dir, optimization_level)
-            logger.info(f"object_file={object_file}")
+                                           object_dir, optimization_level)
+            logger.debug(f"object_file={object_file}")
             disassembly = disassemble(object_file)
-            logger.info(f"object_file={object_file}, disassembly={disassembly}")
+            logger.debug(f"object_file={object_file}, disassembly={disassembly}")
             base_filename = os.path.basename(object_file).split(".")[0]
-            asm_filename = f"{base_filename}_{compiler.compiler_type().name.lower()}_{optimization_level.name}.asm"
+
+            disassembly_id = f"{base_filename}_{compiler.compiler_type().name.lower()}_{optimization_level.name}"
+            diassembly_labels.append(disassembly_id)
+            disassemblies.append(disassembly)
+
+            asm_filename = f"{disassembly_id}.asm"
             disassembly_path = Path(disassembly_dir, asm_filename)
-            logger.info(f"disassembly_path={disassembly_path}")
+            logger.debug(f"disassembly_path={disassembly_path}")
             disassembly_file = open(disassembly_path, 'w')
             disassembly_file.write(disassembly)
             disassembly_file.close()
-
-
     except RuntimeError as e:
         logger.error("Failed to process source files", exc_info=e)
+
+    for model_name in ["Qwen/Qwen3-Embedding-0.6B", "Qwen/Qwen3-Embedding-4B",
+                       "shubharuidas/codebert-base-code-embed-mrl-langchain-langgraph"]:
+        logger.info(f"BEGIN Generating embeddings for {model_name}")
+        model = SentenceTransformer(model_name, device="cuda", model_kwargs={"torch_dtype": torch.bfloat16})
+        embeddings = model.encode(disassemblies, batch_size=8)
+        logger.info(f"END Generating embeddings for {model_name}")
+        del model
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        print('done')
 
 
 if __name__ == "__main__":
